8f685f8bca7531213c7ea0de91be6703d82327d0
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-06-28 21:35:30 +0800

    update

--- src/book/chapter_0.rs
+++ src/book/chapter_0.rs
@@ -2,3 +2,11 @@
 fn test_hello_world() {
     println!("Hello world")
 }
+
+#[test]
+#[rustfmt::skip]
+fn test_func_in_func() {
+    test_hello_world();
+    fn another_func() { println!("this is another func"); }
+    another_func();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// 保持main.rs，否则会有报错
+
+// rust-analyzer
+// cargo check failed to start: Cargo watcher failed, the command produced no valid metadata (exit code: ExitStatus(unix_wait_status(25856))):
+// error: failed to parse manifest at `/Users/chenzhi/Desktop/Rust/tonga_rust_tutorial/Cargo.toml`
+
+// Caused by:
+//   no targets specified in the manifest
+//   either src/lib.rs, src/main.rs, a [lib] section, or [[bin]] section must be present
+
+// Failed to load workspaces.
+
+// cargo run
+// error: failed to parse manifest at `/Users/chenzhi/Desktop/Rust/tonga_rust_tutorial/Cargo.toml`
+
+// Caused by:
+//   no targets specified in the manifest
+//   either src/lib.rs, src/main.rs, a [lib] section, or [[bin]] section must be present
+
 mod book;
 
 #[tokio::main]
