60a5ce03b3f08e783a8c526d4309f5b29f20c84b
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-07-20 23:10:26 +0800

    add struct

--- before_rust/main.c
+++ before_rust/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include "useful_struct.h"
+
+struct Boxea {
+    int index_x;
+    int index_y;
+};
 
 int main() {
     int a = 0;
     int * p = &a;
     *p = 1;
     printf("a = %d\n", a);
+
+    struct Boxea alphae = { 50, 25 };
+    printf("alpha ---> (%d,%d)", alphae.index_x, alphae.index_y);
+
     return 0;
 }
