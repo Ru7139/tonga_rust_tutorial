fc527bfa8fff78e5ee02c0bd83edb90df2a2e9b7
Ru7139 <166471914+Ru7139@users.noreply.github.com>
2025-07-22 22:26:20 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -24,18 +24,69 @@ mod web_teach;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dbg!("Hello");
 
-    let a: u32 = (1..=10)
+    let a: u64 = (1..=10)
         .step_by(1)
         .filter(|x| x % 2 == 1)
         .into_iter()
         .enumerate()
-        .map(|(index, value): (usize, u32)| {
+        .map(|(index, value): (usize, u64)| {
             let num = value.pow(index as u32);
             println!("{}, {}, {}", index, value, num);
             num
         })
         .sum();
-    dbg!(a);
+
+    let a_ox = min_sum_3x3_matrix(a);
+    for row in a_ox.iter() {
+        println!("{:?}", row);
+    }
 
     Ok(())
 }
+
+fn min_sum_3x3_matrix(n: u64) -> [[u64; 3]; 3] {
+    factorize(n)
+        .into_iter()
+        .fold(vec![1; 9], |mut acc, p| {
+            let min_index = acc
+                .iter()
+                .enumerate()
+                .min_by_key(|&(_, &val)| val)
+                .map(|(i, _)| i)
+                .unwrap();
+            acc[min_index] *= p;
+            acc
+        })
+        .chunks(3)
+        .map(|chunk| [chunk[0], chunk[1], chunk[2]])
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap()
+}
+
+fn factorize(mut n: u64) -> Vec<u64> {
+    let mut result = vec![];
+    let mut d = 2;
+
+    while d * d <= n {
+        while n % d == 0 {
+            result.push(d);
+            n /= d;
+        }
+        d += 1;
+    }
+
+    if n > 1 {
+        result.push(n);
+    }
+
+    result
+}
+
+// fn main() {
+//     let n = 360;
+//     let mat = min_sum_3x3_matrix(n);
+//     for row in mat.iter() {
+//         println!("{:?}", row);
+//     }
+// }
